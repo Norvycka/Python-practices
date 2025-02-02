a11de1cc4be1bcc7685cd6fed16728963d8be772
Norvycka <vytenisnorkus@gmail.com>
2025-02-02 19:51:23 +0200

    var_length_args

---
+++ Variable Length Arguments.py
@@ -0,0 +1,9 @@
+def order_food(min_order, *args):
+    print(f"you have ordered {min_order}")
+    #print(args)
+    for item in args:
+        print(f"you have ordered: {item}")
+    print("your food will be delivered in 30mins")
+    print("enjoy da food, BOSS!")
+
+order_food("delicious agurk", "bananana","orange")
